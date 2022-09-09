6995e7ef6c1b545103639b8b79eac6773a896784
ohoCoding <youngho7482@gmail.com>
2022-09-09 22:16:00 +0900

    로그 아웃했을 경우 StatckNavigator 컴포넌트 설정

--- src/navigations/StackNavigator.tsx
+++ src/navigations/StackNavigator.tsx
@@ -9,6 +9,8 @@ import Orders from '../pages/Orders';
 import Home from '../pages/Home';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/reducer';
+import SignUpPhone from '../components/SignUp/SignUpPhone';
+import SignUpAgree from '../components/SignUp/SignUpAgree';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,16 +18,49 @@ const StackNavigator = () => {
   const isLoggedIn = useSelector((state: RootState) => !!state.user.accessToken);
   return (
 
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false
+        }} />
       <Stack.Screen name="SignIn" component={SignIn} />
-      <Stack.Screen name="SignUp" component={SignUp} />
+      {/* <Stack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{
+          title: '회원가입',
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontSize: 20,
+            fontWeight: 'bold',
+          }
+        }} /> */}
       <Stack.Screen name="Orders" component={Orders} />
       <Stack.Screen name="MyPage" component={Settings} />
+      <Stack.Screen
+        name="SignUpAgree"
+        component={SignUpAgree}
+        options={{
+          title: '회원가입',
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontSize: 20,
+            fontWeight: 'bold',
+          }
+        }} />
+      <Stack.Screen
+        name="SignUpPhone"
+        component={SignUpPhone}
+        options={{
+          title: '휴대폰 인증',
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontSize: 20,
+            fontWeight: 'bold',
+          }
+        }} />
     </Stack.Navigator>
 
   );
